16237a61e5169fdff3efb80a05c5ac468424ecb9
user.email : "ateekshsoni@gmail.com <user.email "ateekshsoni@gmail.com>
2024-12-09 16:45:39 +0530

    implement user authentication with cookie management and blacklist token feature

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const connectToDb = require("./db/db.js");
 const userRoutes = require("./routes/user.routes.js");
 
@@ -11,6 +12,7 @@ connectToDb();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
--- BACKEND/routes/user.routes.js
+++ BACKEND/routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { body } = require("express-validator");
 const userController = require("../controllers/user.controllers.js");
+const authMiddleware = require("../middlewares/auth.middleware.js");
 
 router.post(
   "/register",
@@ -21,5 +22,9 @@ router.post('/login' , [
   body('email').isEmail().withMessage('Invalid email'),
   body('password').isLength({min:6}).withMessage('Password must be at least 6 characters long')
 ], userController.loginUser );
+
+router.get('/profile' ,authMiddleware.authUser , userController.getUserProfile);
+
+router.get('/logout' , authMiddleware.authUser , userController.logoutUser);
 
 module.exports = router;
